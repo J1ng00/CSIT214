0d999e788348647c410685e2c5903c85702704fd
jyliew1223 <liewjiayu2022@gmail.com>
2024-04-16 01:20:29 +0800

    Updated 15/4/2024
    Combined both design
    
    1)Sidebar
    The sidebar was removed as it is no longer necessary after the booking section was shifted to the student page.(sidebar.css is still retained)
    
    2)staff
    -added topbar
    -named most color code in CSS files
    -some changes were made in CSS files to merge both designs
    
    3)Student
    -design was changed based on the discussion.
    -booking page has been moved from another HTML file to this one.(booking.html and booking.css is still retained)
    -information section has been moved to the top bar.
    
    4)Payment
    -changes were made to merge both design
    
    5)Topbar
    -an information btn has been added
    -a dropdown tab has been added for information section
    
    6)information tab
    -let user access their account setting and view upcoming bookings(not yet develop)
    -now used to switch between HTML files
    
    7)Calender.css
    -A CSS file that separates calendar styles from JingLong's staff.css for easier future use of the calendar.(simply copy the code and include css and javascript should works)
    -some extra lines were added for transition effects when hovering over it with the mouse.

--- information_bar_script.js
+++ information_bar_script.js
@@ -1,9 +1,11 @@
-function openInformationbar() {
-    document.getElementById("information_bar").style.right = "0";
-    document.querySelector(".content").style.marginright = "0px";
-}
+function ToggleInformationbar() {
+    var userInfoBar = document.getElementById("information_bar");
+    var contentHeight = userInfoBar.scrollHeight + 'px'; // Get the content height
 
-function closeInformationbar() {
-    document.getElementById("information_bar").style.right = "-300px";
-    document.querySelector(".content").style.marginright = "0";
+    // Toggle max-height to reveal or hide the content
+    if (userInfoBar.style.maxHeight === "0px" || !userInfoBar.style.maxHeight) {
+        userInfoBar.style.maxHeight = contentHeight;
+    } else {
+        userInfoBar.style.maxHeight = "0px";
+    }
 }
