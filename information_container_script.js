cddffe6c7332d4bab09799ddd2e4bb6f718f4dd8
J1ng00 <wongjl@hotmail.com>
2024-04-22 11:55:58 +0800

    TopBarEdits

--- information_container_script.js
+++ information_container_script.js
@@ -13,3 +13,32 @@ function ToggleInformationbar() {
 function logout(){
     window.location.href = "index.html";
 }
+
+var currentPage = window.location.pathname;
+
+function generateMenu() {
+    var menu = document.querySelector(".menu");
+    
+    // Clear existing menu items
+    menu.innerHTML = "";
+
+    // Generate menu items based on the current page
+    if (currentPage.includes("student.html")) {
+        menu.innerHTML += '<a href="../payment.html"><li>Payment</li></a>';
+        menu.innerHTML += '<a href="../staff.html"><li>Staff</li></a>';
+        menu.innerHTML += '<a href="../index.html"><li>Index</li></a>';
+    } else if (currentPage.includes("staff.html")) {
+        menu.innerHTML += '<a href="../admin.html"><li>Admin</li></a>';
+        menu.innerHTML += '<a href="../student.html"><li>Student</li></a>';
+        menu.innerHTML += '<a href="../index.html"><li>Index</li></a>';
+    } else {
+        // Default menu items for other pages
+        menu.innerHTML += '<a href="../student.html"><li>Student</li></a>';
+        menu.innerHTML += '<a href="../staff.html"><li>Staff</li></a>';
+        menu.innerHTML += '<a href="../payment.html"><li>Payment</li></a>';
+    }
+}
+
+// Call the generateMenu function when the page loads
+generateMenu();
+
